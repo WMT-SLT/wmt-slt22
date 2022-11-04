a8cd0e67cf3ca6881ea9468ea99d062c2f7d4e8a
Eleftherios Avramidis <eleftherios.avramidis@dfki.de>
2022-11-04 20:48:29 +0100

    Removed tree from older repository

--- human_evaluation/generate-ranking.sh
+++ human_evaluation/generate-ranking.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-scripts/ComputeWMT21Results.py --separate-domains --task-type Document --csv-file scores/aggregated/all.csv foo | tee ranking.log
+python scripts/ComputeWMTSLT22Results.py --separate-domains --task-type Document --csv-file scores/aggregated/all.csv foo | tee ranking.log
